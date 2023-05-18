bacd77d8357b032c9656db43fdd0bb56f1300f06
farzad <farza@DESKTOP-UDVL642>
2023-05-17 20:08:17 -0400

    Comments added to the FileValidationResult Model

--- NameSorter/Models/FileValidatorResult.cs
+++ NameSorter/Models/FileValidatorResult.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace NameSorter.Models
 {
+    /// <summary>
+    /// This model created to use for the result that methods return after validating the file path
+    /// </summary>
     public class FileValidatorResult
     {
+        /// <summary>
+        /// If file is valid set true; else false
+        /// </summary>
         public bool FileIsValid { get; set; }
+
+        /// <summary>
+        /// Add a message based n the result to print in the console
+        /// </summary>
         public string ValidatorMessage { get; set; }
     }
 }
