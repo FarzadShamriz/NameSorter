aa5139021c9031b1fcf6b2cdac9365d6a5907b25
farzad <farza@DESKTOP-UDVL642>
2023-05-17 20:02:42 -0400

    Added write to file in the FIleHelper and comments for the class and methods. Handled exception in the main method.

--- NameSorter/Helpers/FileHelper.cs
+++ NameSorter/Helpers/FileHelper.cs
@@ -1,5 +1,6 @@
 ﻿using NameSorter.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,30 +8,50 @@ using System.Threading.Tasks;
 
 namespace NameSorter.Helpers
 {
+    /// <summary>
+    /// This class contains the helper methods related to the file
+    /// </summary>
     public class FileHelper
     {
         private static readonly string _fileNotFoundMsg = "File is not exist in the entered path.";
         private static readonly string _fileFoundMsg = "File is exist.";
+        private static readonly string _filePathEmptyMsg = "Enter the file name.";
+        private static readonly string _fileExtErrorMsg = "Enter the path of the text file.";
+        private static readonly string _destFilePath = "sorted-names-list.txt";
 
         /// <summary>
-        /// 
+        ///  This method is validating the file path entered by the user 
+        ///  and returns an object that contains isValid flag as boolean and a message for the UI purposes
         /// </summary>
-        /// <param name="filePath"></param>
-        /// <returns></returns>
+        /// <param name="filePath">String input parameter to validate the file</param>
+        /// <returns>Returns an object that contains isValid flag as boolean and a message for the UI purposes</returns>
         public static FileValidatorResult ValidateFileByPath(string filePath)
         {
-            if (!File.Exists(filePath))
+
+            if (string.IsNullOrEmpty(filePath.Replace(" ", "")))
+            {
+                return new FileValidatorResult { FileIsValid = false, ValidatorMessage = _filePathEmptyMsg };
+            }
+            else if (!Path.GetExtension(filePath).ToLower().Equals(".txt"))
+            {
+                return new FileValidatorResult { FileIsValid = false, ValidatorMessage = _fileExtErrorMsg };
+            }
+            else if (!File.Exists(filePath))
             {
                 return new FileValidatorResult { FileIsValid = false, ValidatorMessage = _fileNotFoundMsg };
+
             }
+
             return new FileValidatorResult { FileIsValid = true, ValidatorMessage = _fileFoundMsg }; ;
         }
 
         /// <summary>
-        /// 
+        /// This method is responsible to read the file line by line 
+        /// from the path entered as an input,
+        /// and create a person object based on the data read from the file.
         /// </summary>
-        /// <param name="filePath"></param>
-        /// <returns></returns>
+        /// <param name="filePath">String input parameter to read the file</param>
+        /// <returns>Returns a list of person objects</returns>
         public static List<Person> SerializeFileToPersonList(string filePath)
         {
             var personList = new List<Person>();
@@ -40,7 +61,7 @@ namespace NameSorter.Helpers
             {
                 foreach (var personItem in extractedLinesFromFile.ToList())
                 {
-                    if (personItem.Trim().Length > 0)
+                    if (personItem.Trim().Length > 0 && personItem.Trim().Split(' ').Count() <= 4)
                     {
                         var lastName = personItem.Trim().Split(' ').Last();
                         var firstName = personItem.Replace(lastName, "").Trim();
@@ -51,6 +72,38 @@ namespace NameSorter.Helpers
                 }
             }
             return personList;
+        }
+
+        /// <summary>
+        /// This class is responsible to write the list of person in a file.
+        /// It opens a file and take each person object to concatinate the 
+        /// first name and last name to write it in a line.
+        /// </summary>
+        /// <param name="personList">List of person objects to write one by one in a file</param>
+        /// <returns>Return true or false based on the process.</returns>
+        public static bool WritePersonListInFile(List<Person> personList)
+        {
+            TextWriter sortedFile = new StreamWriter(_destFilePath);
+            try
+            {
+                foreach (Person person in personList)
+                {
+                    var fullName = $"{person.FirstName} {person.LastName}";
+                    Console.WriteLine(fullName);
+                    sortedFile.WriteLine(fullName);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                sortedFile.Close();
+            }
+            return true;
         }
     }
 }
--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -2,29 +2,48 @@
 using NameSorter.Models;
 
 
-//Check arg0
-string inputFilePath = Environment.GetCommandLineArgs()[0];
-var fileValidationResult = FileHelper.ValidateFileByPath(inputFilePath);
 
-if (fileValidationResult.FileIsValid)
+try
 {
-    Console.WriteLine("================================", ConsoleColor.Green);
-    Console.WriteLine(fileValidationResult.ValidatorMessage, ConsoleColor.Green);
-    Console.WriteLine("================================", ConsoleColor.Green);
-
-    List<Person> serializedPersonList = FileHelper.SerializeFileToPersonList(inputFilePath);
-
-    if (serializedPersonList.Count < 1)
+    if (Environment.GetCommandLineArgs().Length > 1)
     {
-        Console.WriteLine("================================", ConsoleColor.Red);
-        Console.WriteLine("File is empty.", ConsoleColor.Red);
-        Console.WriteLine("================================", ConsoleColor.Red);
+        string inputFilePath = Environment.GetCommandLineArgs()[1];
+        var fileValidationResult = FileHelper.ValidateFileByPath(inputFilePath);
+        Console.WriteLine(inputFilePath);
+
+        if (fileValidationResult.FileIsValid)
+        {
+            Console.WriteLine("================================", ConsoleColor.Green);
+            Console.WriteLine(fileValidationResult.ValidatorMessage, ConsoleColor.Green);
+            Console.WriteLine("================================", ConsoleColor.Green);
+
+            List<Person> serializedPersonList = FileHelper.SerializeFileToPersonList(inputFilePath);
+
+            if (serializedPersonList.Count < 1)
+            {
+                Console.WriteLine("================================", ConsoleColor.Red);
+                Console.WriteLine("File is empty.", ConsoleColor.Red);
+                Console.WriteLine("================================", ConsoleColor.Red);
+            }
+            else
+            {
+                List<Person> sortedPersonList = PersonHelper.SortPersonListByLastNameThenFirstName(serializedPersonList);
+                FileHelper.WritePersonListInFile(sortedPersonList);
+            }
+
+
+        }
     }
     else
     {
-        List<Person> sortedList = PersonHelper.SortPersonListByLastNameThenFirstName(serializedPersonList);
-
+        Console.WriteLine("================================", ConsoleColor.Red);
+        Console.WriteLine("Wrong command.", ConsoleColor.Red);
+        Console.WriteLine("================================", ConsoleColor.Red);
     }
-
-
 }
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
+
+
